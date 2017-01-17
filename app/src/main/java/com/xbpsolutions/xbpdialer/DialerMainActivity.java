844aa427e354a9bad90639d989b1d9c61f65c413
dhruvil <dd.droid@yahoo.com>
2017-01-17 18:47:31 +0530

    Initial commit

---
+++ app/src/main/java/com/xbpsolutions/xbpdialer/DialerMainActivity.java
@@ -0,0 +1,13 @@
+package com.xbpsolutions.xbpdialer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DialerMainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_dialer_main);
+  }
+}
